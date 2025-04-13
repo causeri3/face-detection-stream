6bcff923108822f6f5bd6fbe8366db3e5cdf8ac0
causeri3 <vanessaca@web.de>
2025-04-13 12:46:59 +0200

    initial commit

---
+++ utils/track.py
@@ -0,0 +1,22 @@
+from deep_sort_realtime.deepsort_tracker import DeepSort
+import numpy as np
+
+tracker = DeepSort(
+    # works way better than default
+    embedder='clip_RN50x16' ,
+    # clip does not run via my silicon arch, dunno if there is a mps setting, set to true on Windows, see if it works
+    embedder_gpu = False)
+
+def get_ids(bbs: tuple[list, float, str], frame: np.ndarray) -> list:
+    """
+    :param bbs: tuple of ( [left,top,w,h], confidence, detection_class)
+    :param frame: np.ndarray"""
+    tracks = tracker.update_tracks(bbs, frame=frame) # bbs expected to be a list of detections, each in tuples of ( [left,top,w,h], confidence, detection_class )
+    ids = []
+    for track in tracks:
+        if not track.is_confirmed():
+            ids.append("-")
+            #continue
+        ids.append(track.track_id)
+        # ltrb = track.to_ltrb()
+    return ids
