dc3f5868b79c17956d8f29e05271068276b6f1e8
causeri3 <vanessaca@web.de>
2025-04-14 18:09:40 +0200

    get target coordinates basing on detections and tracking

--- utils/predict.py
+++ utils/predict.py
@@ -1,6 +1,7 @@
 from utils.payloads import json_payload, image_payload
 from utils.args import get_args
 from utils.track import get_ids
+from utils.target import EyesTarget
 
 
 import numpy as np
@@ -13,6 +14,8 @@ from supervision import Detections
 from PIL import Image
 
 args, unknown = get_args()
+
+eyes_target = EyesTarget()
 
 def convert_to_model_format(frame: np.ndarray):
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -34,7 +37,7 @@ def predict(image: np.ndarray,
     """Gives object detection for one image from yolo v7 via onnx runtime session
     :param image: Input image in bytes or as np array
     :param model: YOLO model
-    :param return_json: returning json payload with results (tags, boxes, confidences)
+    :param return_json: returning json payload with results
     :param return_image: Returning image with labels, confidences and boxes as bytes
     :returns If none of the above return options is chosen the image will open locally"""
 
@@ -47,19 +50,19 @@ def predict(image: np.ndarray,
 
     logging.debug("Detected {} objects".format(len(detected_objects)))
 
-    if return_json and return_image:
-        json_output = json_payload(detected_objects)
-        track_ids = get_ids(json_output["bbs"], image)
-        bytes_output = image_payload(detected_objects, image, track_ids)
+    json_output = json_payload(detected_objects)
+    track_ids = get_ids(json_output["bbs"], image)
+    [d.update({'id': i}) for d, i in zip(json_output["objects"], track_ids)]
+    x, y = eyes_target.update(json_output["objects"], image)
+    json_output.update({'target_coordinates_xy': (x,y)})
 
-        end_time = time.time()
-        logging.debug("One detection event took {:.2f} seconds".format(end_time - start_time))
+    end_time = time.time()
+    logging.debug("One detection event took {:.2f} seconds".format(end_time - start_time))
+    if return_image:
+        bytes_output = image_payload(detected_objects, image, track_ids, (x, y))
         return bytes_output, json_output
 
-    if return_json:
-        json_output = json_payload(detected_objects)
-        end_time = time.time()
-        logging.debug("One detection event took {:.2f} seconds".format(end_time - start_time))
-        return json_output
+    return json_output
+
 
 
