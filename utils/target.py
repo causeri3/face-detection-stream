5cd9872b7b4d0da650949dc09462a047b447074f
causeri3 <vanessaca@web.de>
2025-04-29 11:02:05 +0300

    changes in follow concept

--- utils/target.py
+++ utils/target.py
@@ -1,16 +1,16 @@
 """
-There are two states:
-STARE and BORED
-They both take a random amount of time in between a max and min chosen range. Once time is up, they change.
-Except, if there are no face ids, it will always be in BORED state.
+There are three states:
+ALONE, BASIC_BITCH_STARE, and CLOSEUP_STARE.
 
-In BORED:
-the target point does a random walk with the maximum distance of 2*10% of the screen width
-In STARE:
-If no target id chosen or chosen one is not available, chooses one at random. Target point follows center of bounding box
+ALONE:
+IF there are no faces detected: The target point does a random walk across the screen. ALONE continues indefinitely until a face with an ID appears, at which point it switches to BASIC_BITCH_STARE or CLOSEUP_STARE.
 
-Little extra:
-Once a face comes closer than 25% of the screen, it gets stared at, which ever state we are in.
+BASIC_BITCH_STARE:
+The target point follows the center in the upper third of a randomly chosen face ID. This state lasts for a random time range. If the time runs out or the face disappears, a new face is picked, and a new timer starts.
+
+CLOSEUP_STARE:
+Triggered immediately whenever a face larger than face_size_threshold of the screen appears. The target point locks onto a random chosen close-up face and keeps following it indefinitely. If the current close-up face disappears, a new close face is chosen randomly. It only exits CLOSEUP_STARE if no close face is detected.
+
 """
 
 import time
@@ -23,40 +23,44 @@ from utils.render import PINK
 
 class EyesTarget:
     def __init__(self):
-        # "BORED" or "STARE"
-        self.state = "BORED"
+        # "ALONE" or "BASIC_BITCH_STARE" or "CLOSEUP_STARE"
+        self.state = "ALONE"
 
         # the face id we are currently staring at
         self.current_id = None
+        self.valid_faces = []
 
         self.target_x = 0
         self.target_y = 0
 
         #_____________ Params to play with __________________ #
-        self.bored_min_time = 5
-        self.bored_max_time = 15
-        self.stare_min_time = 20
-        self.stare_max_time = 40
+        self.stare_min_time = 20 #sec
+        self.stare_max_time = 40 #sec
         self.face_size_threshold = 0.25  # % of screen area
         self.pause_min_time = 1  # Minimum pause time in seconds
         self.pause_max_time = 3  # Maximum pause time in seconds
         #______________________________________________________ #
 
         self.state_start_time = time.time()
-        # duration of state
-        self.state_duration = random.uniform(self.bored_min_time, self.bored_max_time)
+        self.state_duration = 0
         self.last_move_time = time.time()
 
 
-    def _reset_state(self, new_state: str):
+    def _reset_state(self,
+                     new_state: str):
         """Helper to reset timings when changing states."""
         self.state = new_state
         self.state_start_time = time.time()
-        if new_state == "BORED":
-            self.state_duration = random.uniform(self.bored_min_time, self.bored_max_time)
-        else:  # STARE
+        if self.state == "BASIC_BITCH_STARE":
             self.state_duration = random.uniform(self.stare_min_time, self.stare_max_time)
+        else:
+            self.state_duration = 0
+        self.current_id = self.pick_random_id()
 
+    def pick_random_id(self) -> int | None:
+        if not self.valid_faces:
+            return None
+        return random.choice(self.valid_faces)['id']
 
     def random_walk_with_pause(self,
                                width: float,
@@ -116,12 +120,12 @@ class EyesTarget:
         screen_area = screen_w * screen_h
 
         # filters out faces that actually have an ID, if deepsort track.is_confirmed()
-        valid_faces = [f for f in faces if f['id'] is not None]
+        self.valid_faces = [f for f in faces if f['id'] is not None]
 
-        ids_available = (len(valid_faces) > 0)
+        ids_available = (len(self.valid_faces) > 0)
 
         close_faces = []
-        for f in valid_faces:
+        for f in self.valid_faces:
             w = f['box']['width']
             h = f['box']['height']
             area = w * h
@@ -131,51 +135,44 @@ class EyesTarget:
         # ______________________ react to close face _______________________ #
         react = (len(close_faces) > 0)
         if react:
+            self.valid_faces = close_faces
             # If multiple large faces, see if current face is still among them
             current_big = any(f['id'] == self.current_id for f in close_faces)
             if not current_big:
-                chosen_face = random.choice(close_faces)
-                self.current_id = chosen_face['id']
-            # switch to STARE if not already
-            if self.state != "STARE":
-                self._reset_state("STARE")
+                self.current_id = self.pick_random_id()
+            if self.state != "CLOSEUP_STARE":
+                self._reset_state("CLOSEUP_STARE")
+        else:
+            if self.state == "CLOSEUP_STARE":
+                self._reset_state("BASIC_BITCH_STARE" if ids_available else "ALONE")
 
         # ______________________ chose states _______________________ #
-        if self.state == "BORED":
-            if time_in_state < self.state_duration:
-                pass
-            else:
-                # BORED time is up
-                if ids_available:
-                    chosen_face = random.choice(valid_faces)
-                    self.current_id = chosen_face['id']
-                    self._reset_state("STARE")
-                else:
-                    # No faces -> remain bored (reset bored timer)
-                    self._reset_state("BORED")
 
-        elif self.state == "STARE":
-            still_see_current = any(f['id'] == self.current_id for f in valid_faces)
+        if not ids_available and not self.state == "ALONE":
+            self._reset_state("ALONE")
+
+        if self.state == "ALONE" and ids_available:
+            self._reset_state("BASIC_BITCH_STARE")
+
+        elif self.state == "BASIC_BITCH_STARE":
+            still_see_current = any(f['id'] == self.current_id for f in self.valid_faces)
             if time_in_state < self.state_duration and still_see_current:
                 pass
             else:
-                # STARE time is up OR we lost sight of current face
-                self._reset_state("BORED")
+                # stare time is up OR we lost sight of current face, pick  new id at random
+                self._reset_state("BASIC_BITCH_STARE")
 
         # ______________________ define new target spot _______________________ #
-        if self.state == "BORED":
+        if self.state == "ALONE":
             if int(time_in_state) % 2 == 0:
-                # every 2 seconds, pick a new random location
-                # self.target_x = random.uniform(0, screen_w)
-                # self.target_y = random.uniform(0, screen_h)
-
                 # Every 2 seconds, pick a new random location using random walk
                 self.target_x, self.target_y = self.random_walk_with_pause(screen_w, screen_h, self.target_x, self.target_y)
 
 
-        elif self.state == "STARE":
+
+        elif self.state in ["BASIC_BITCH_STARE", "CLOSEUP_STARE"]:
             # Find the face with self.current_id, if it exists
-            for f in valid_faces:
+            for f in self.valid_faces:
                 if f['id'] == self.current_id:
                     # the center of that face's box
                     self.target_x = f['box']['x'] + f['box']['width'] / 2
@@ -183,10 +180,10 @@ class EyesTarget:
                     break
 
         # for debugging
-        screen_logs = """{} Target ID: {} Countdown: {:.0f} ids_available: {}, any close faces: {}""".format(
+        screen_logs = """{} Target ID: {} | Countdown or Time in State: {:.0f} | ids_available: {} | any close faces: {}""".format(
                             self.state,
                                     self.current_id,
-                                    self.state_duration - time_in_state,
+                                    - (self.state_duration - time_in_state),
                                     ids_available,
                                     (len(close_faces) > 0)
                                     )
@@ -201,3 +198,4 @@ class EyesTarget:
         )
 
         return self.target_x, self.target_y, self.current_id, self.state
+
